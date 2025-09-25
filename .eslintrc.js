f752e28d87f6796d5111ee6aee173cddafb115a1
blencm <35551174+blencm@users.noreply.github.com>
2025-09-25 09:24:04 -0500

    Publish react-helmet-next

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import('@types/eslint').Linter.BaseConfig}
+ */
+module.exports = {
+  extends: [
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        useTabs: false,
+        tabWidth: 2,
+        printWidth: 100,
+      },
+    ],
+    'testing-library/render-result-naming-convention': 'off',
+  },
+  settings: {
+    jest: {
+      version: 27,
+    },
+  },
+};
